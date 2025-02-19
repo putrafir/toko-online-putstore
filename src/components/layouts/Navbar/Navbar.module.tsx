3bce967dede25bce8ffcc77b175e6b8d102bd8d1
putrafir <firdausputra0212@gmail.com>
2025-02-20 00:12:33 +0700

    fix: navbar

--- src/components/layouts/Navbar/Navbar.module.tsx
+++ src/components/layouts/Navbar/Navbar.module.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const { data } = useSession();
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 ">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <a
             href="https://flowbite.com"
@@ -15,14 +15,14 @@ const Navbar = () => {
               className="h-8"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap ">
               Flowbite
             </span>
           </a>
           <div className="flex items-center space-x-6 rtl:space-x-reverse">
             <a
               href="tel:5541251234"
-              className="text-sm  text-gray-500 dark:text-white hover:underline"
+              className="text-sm  text-gray-500 hover:underline"
             >
               {data?.user?.email}
             </a>
@@ -31,47 +31,38 @@ const Navbar = () => {
               onClick={() => {
                 data ? signOut() : signIn();
               }}
-              className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
+              className="text-sm  text-blue-600  hover:underline"
             >
               {data ? "Logout" : "Login"}
             </a>
           </div>
         </div>
       </nav>
-      <nav className="bg-gray-50 dark:bg-gray-700">
+      <nav className="bg-gray-50 ">
         <div className="max-w-screen-xl px-4 py-3 mx-auto">
           <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
               <li>
                 <a
                   href="#"
-                  className="text-gray-900 dark:text-white hover:underline"
+                  className="text-gray-900 hover:underline"
                   aria-current="page"
                 >
                   Home
                 </a>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline"
-                >
+                <a href="#" className="text-gray-900 hover:underline">
                   Company
                 </a>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline"
-                >
+                <a href="#" className="text-gray-900 hover:underline">
                   Team
                 </a>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="text-gray-900 dark:text-white hover:underline"
-                >
+                <a href="#" className="text-gray-900 hover:underline">
                   Features
                 </a>
               </li>
