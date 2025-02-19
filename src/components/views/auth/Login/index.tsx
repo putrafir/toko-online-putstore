85906d1e0811764d62bc8bf1ff7a2102f0f2385a
putrafir <firdausputra0212@gmail.com>
2025-02-19 17:50:13 +0700

    fix: callback in login google

--- src/components/views/auth/Login/index.tsx
+++ src/components/views/auth/Login/index.tsx
@@ -42,7 +42,7 @@ const LoginView = () => {
         <h1 className=" mb-8 text-3xl text-center text-blue-700">Login</h1>
         <button
           type="button"
-          onClick={() => signIn("google", callbackUrl)}
+          onClick={() => signIn("google", { callbackUrl })}
           className="text-white w-full justify-center bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
         >
           <svg
