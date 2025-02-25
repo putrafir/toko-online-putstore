60026b1b6bf770bd80d99a53560d24543b3e560f
putrafir <firdausputra0212@gmail.com>
2025-02-25 22:17:16 +0700

    fix: navbar for auth and admin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
-import Navbar from "@/components/layouts/Navbar/Navbar.module";
+import Navbar from "@/components/layouts/Navbar";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+
+const disableNavbar = ["auth", "admin"];
 
 export default function App({
   Component,
   pageProps: { session, pageProps },
 }: AppProps) {
+  const { pathname } = useRouter();
+
   return (
     <SessionProvider session={session}>
-      <Navbar></Navbar>
+      {!disableNavbar.includes(pathname.split("/")[1]) && <Navbar />}
       <Component {...pageProps} />
     </SessionProvider>
   );
